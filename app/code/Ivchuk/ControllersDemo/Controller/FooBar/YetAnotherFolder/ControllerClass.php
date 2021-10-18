c44a4c0d8d3c007f65353bcd1fe5007ad0cb6b9c
SerhiiIvchuk <sergii.i@open-tag.com>
2021-10-18 21:38:33 +0300

    3-magento-controllers-routing: json data transmitting

--- app/code/Ivchuk/ControllersDemo/Controller/FooBar/YetAnotherFolder/ControllerClass.php
+++ app/code/Ivchuk/ControllersDemo/Controller/FooBar/YetAnotherFolder/ControllerClass.php
@@ -4,38 +4,43 @@ declare(strict_types=1);
 namespace Ivchuk\ControllersDemo\Controller\FooBar\YetAnotherFolder;
 
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\Result\Json;
 
 class ControllerClass implements
     \Magento\Framework\App\Action\HttpGetActionInterface
 {
     private \Magento\Framework\App\RequestInterface $request;
-    private \Magento\Framework\Controller\Result\RawFactory $rawFactory;
+    private \Magento\Framework\Controller\Result\JsonFactory $jsonFactory;
 
     /**
      * @param \Magento\Framework\App\RequestInterface $request
-     * @param \Magento\Framework\Controller\Result\RawFactory $rawFactory
+     * @param \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
      */
     public function __construct(
         \Magento\Framework\App\RequestInterface $request,
-        \Magento\Framework\Controller\Result\RawFactory $rawFactory
+        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
     ) {
         $this->request = $request;
-        $this->rawFactory = $rawFactory;
+        $this->jsonFactory = $jsonFactory;
     }
 
     /**
-     * @return Raw
+     * @return Json
      */
-    public function execute(): Raw
+    public function execute(): Json
     {
+        return $this->jsonFactory->create()
+            ->setData([
+                'parameter-name-1' => $this->request->getParam('parameter-name-1', ''),
+                'demo_int' => (int) $this->request->getParam('demo-int', 10)
+            ]);
 
-      $result=$this->rawFactory->create();
-       return $result->setHeader('Content-Type','text/plain')
-           ->setContents(
-               'Demo int:' . (int) $this->request->getParam('demo-int', 10) . '<br>' .
-                $this->request->getParam('parameter-name-1') . '<br>'
-           );
+//      $result=$this->rawFactory->create();
+//       return $result->setHeader('Content-Type','text/json')
+//           ->setContents(
+//               'Demo int:' . (int) $this->request->getParam('demo-int', 10) . '<br>' .
+//                $this->request->getParam('parameter-name-1') . '<br>'
+//           );
 
 //        echo 'Demo int:' . (int) $this->request->getParam('demo-int', 10) . '<br>';
 //        echo 'parameter-name-1:' . $this->request->getParam('parameter-name-1') . '<br>';
@@ -44,4 +49,3 @@ class ControllerClass implements
 //        exit;
     }
 }
-
